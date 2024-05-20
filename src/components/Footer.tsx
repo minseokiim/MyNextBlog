ceb4a17b58e2f5ca9cbd39272965211b49856742
minseokiim <alstj991226@naver.com>
2024-05-20 19:47:50 +0900

    design: 레이아웃 골격 스타일링

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Footer() {
-  return <p>{"All right Reserved"}</p>;
+  return (
+    <p className="bg-slate-900 text-white py-2 text-sm">
+      {"All right Reserved"}
+    </p>
+  );
 }
