7170e9110e75fc1be825b7b657f55f3d6484aa83
minseokiim <alstj991226@naver.com>
2024-05-21 18:15:28 +0900

    feat: 카드컴포넌트에서 데이터 뿌려주기

--- src/components/CardPost.tsx
+++ src/components/CardPost.tsx
@@ -1,6 +1,26 @@
+import { Post } from "@/api/posts";
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
-export default function CardPost() {
-  return <div></div>;
+type Props = { post: Post };
+export default function CardPost({
+  post: { title, description, date, category, path },
+}: Props) {
+  return (
+    <Link href={`/posts/${path}`}>
+      <Image
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={300}
+        height={200}
+      />
+      <div>
+        <time>{date.toString()}</time>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <span>{category}</span>
+      </div>
+    </Link>
+  );
 }
-//여기부터(9.10)
--- src/components/GridPosts.tsx
+++ src/components/GridPosts.tsx
@@ -1,11 +1,14 @@
 import { Post } from "@/api/posts";
 import React from "react";
+import CardPost from "./CardPost";
 
 export default function GridPosts({ posts }: { posts: Post[] }) {
   return (
     <ul>
       {posts.map((post) => (
-        <li key={post.path}>{post.title}</li>
+        <li key={post.path}>
+          <CardPost post={post} />
+        </li>
       ))}
     </ul>
   );
