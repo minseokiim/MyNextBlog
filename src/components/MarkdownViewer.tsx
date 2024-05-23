e70fe0a3547fa4f760e65114773aa0ae84eb358d
minseokiim <alstj991226@naver.com>
2024-05-23 19:48:22 +0900

    feat: syntax-highlighter 적용

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <Markdown remarkPlugins={[remarkGfm]} className="prose lg:prose-xl">
+    <Markdown
+      remarkPlugins={[remarkGfm]}
+      className="prose lg:prose-xl"
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              {...rest}
+              style={materialDark}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {content}
     </Markdown>
   );
