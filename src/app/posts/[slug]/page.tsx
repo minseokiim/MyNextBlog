6fe624712c68cd3e7b2f5e3dd646522050a07b96
minseokiim <alstj991226@naver.com>
2024-05-23 19:26:10 +0900

    design: 마크다운 컴포넌트 스타일링

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { getPostData } from "@/api/posts";
 import MarkdownViewer from "@/components/MarkdownViewer";
 import React from "react";
+import Image from "next/image";
+import { AiTwotoneCalendar } from "react-icons/ai";
 
 type Props = {
   params: {
@@ -8,12 +10,29 @@ type Props = {
   };
 };
 export default async function page({ params: { slug } }: Props) {
-  const post = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
 
   return (
-    <>
-      <h1>{post.title}</h1>
-      <MarkdownViewer content={post.content} />
-    </>
+    <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+      <Image
+        className="w-full rounded-2xl h-1/5 max-h-[500px]"
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end text-blue-900">
+          <AiTwotoneCalendar />
+          <p className="font-semibold ml-2">{date.toString()}</p>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-semibold">{description}</p>
+
+        {/* 간격을 주기 위해 div 사용 */}
+        <div className="w-44 border-2 border-blue-900 mt-4 mb-2"></div>
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
