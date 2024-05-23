878b0f5515221f3d56bff22aace0bd83cc940ace
minseokiim <alstj991226@naver.com>
2024-05-23 19:03:18 +0900

    feat: tailwindcss/typography 적용

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -3,5 +3,9 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 export default function MarkdownViewer({ content }: { content: string }) {
-  return <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>;
+  return (
+    <Markdown remarkPlugins={[remarkGfm]} className="prose lg:prose-xl">
+      {content}
+    </Markdown>
+  );
 }
